5a7afef61739e2dbe74e1a3d19d2e2788d1679a4
Cedric Marie <djced96@gmail.com>
2022-06-11 15:43:29 +0400

    changed routes for booking & review

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
   get 'users/edit'
   get 'users/update'
-  get 'review/new'
-  get 'review/create'
-  get 'review/update'
-  get 'review/edit'
-  get 'review/destroy'
-  get 'review/index'
-  get 'review/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/update'
-  get 'bookings/edit'
-  get 'bookings/destroy'
-  get 'bookings/index'
-  get 'bookings/show'
+  
+  resources :booking do
+    resources :review
+  end
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
