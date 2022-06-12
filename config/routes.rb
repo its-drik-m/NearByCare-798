aacc2c984cbc12db4a11d6cccb1893beb32f9fc3
Vanessa F <vfigaro02@gmail.com>
2022-06-12 13:01:41 +0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'testing', to: 'pages#test'
 
-  resources :users, only: %i[edit update] do
-    resources :patients, only: %i[edit update show] do
-      resources :bookings, only: %i[new create index show destroy]
-      resources :reviews, only: %i[new create index show destroy]
-    end
-    resources :carers, only: %i[index edit update show] do
-      resources :bookings, only: %i[new create index show destroy]
-      resources :reviews, only: %i[new create index show destroy]
-    end
-  end
+  # resources :users, only: %i[edit update] do
+  #  resources :patients, only: %i[edit update show] do
+  #  resources :bookings, only: %i[new create index show destroy]
+  #  resources :reviews, only: %i[new create index show destroy]
+  # end
+  # resources :carers, only: %i[index edit update show] do
+  #   resources :bookings, only: %i[new create index show destroy]
+  #   resources :reviews, only: %i[new create index show destroy]
+  # end
+  # end
 end
