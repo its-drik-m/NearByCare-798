677f42942c751893cbc8ea32b58848041eddf37b
Vanessa F <vfigaro02@gmail.com>
2022-06-18 13:23:19 +0400

    Update registrations_controller.rb

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
   # redirecting to role selection after signup
-
   private
   def after_sign_up_path_for(_resources)
     edit_user_path(current_user.id)
