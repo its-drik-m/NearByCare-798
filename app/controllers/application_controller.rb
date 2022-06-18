618ec239c568ec97be95f201fdfffc710d061c6d
Vanessa F <vfigaro02@gmail.com>
2022-06-18 13:15:01 +0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
 #     end
 
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name phone_number role carer patient])
   end
