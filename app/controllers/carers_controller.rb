97f837f1914a34929efad190b2c8b52a7f09d0cd
vanessa figaro <vfigaro02@gmail.com>
2022-06-24 12:09:25 +0400

    Search bar ajaxified

--- app/controllers/carers_controller.rb
+++ app/controllers/carers_controller.rb
@@ -10,6 +10,11 @@ class CarersController < ApplicationController
     else
       @carers = Carer.all
     end
+
+    respond_to do |format|
+      format.html
+      format.text { render partial: 'list.html', locals: { carers: @carers }, formats: [:html] }
+    end
   end
 
   def new
