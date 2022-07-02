69ca9885db783de2830764297621d7c766fb5236
Karishma Jogoo <karishma_jogoo@hotmail.com>
2022-07-02 08:54:01 +0400

    search array

--- app/controllers/carers_controller.rb
+++ app/controllers/carers_controller.rb
@@ -5,8 +5,8 @@ class CarersController < ApplicationController
   def index
     # @carers = Carer.order(first_name: :desc)
     # if params[:query].present?
-    sql_query = "region ILIKE :region AND specialty ILIKE :specialty"
-    region_query = "specialty ILIKE :specialty"
+    sql_query = "region ILIKE :region AND specialty @> :specialty"
+    region_query = "specialty @> :specialty"
     # @carers = Carer.where(sql_query, query: "%#{params[:query]}%")
     # else
     #   @carers = Carer.all
@@ -15,9 +15,9 @@ class CarersController < ApplicationController
     @specialty = params[:specialty]
 
     if @region == "all"
-      @carers = Carer.where(region_query, specialty: "%#{params[:specialty]}%")
+      @carers = Carer.where(region_query, specialty: "{#{params[:specialty]}}")
     else
-      @carers = Carer.where(sql_query, region: "%#{params[:region]}%", specialty: "%#{params[:specialty]}%")
+      @carers = Carer.where(sql_query, region: "%#{params[:region]}%", specialty: "{#{params[:specialty]}}")
     end
 
     respond_to do |format|
