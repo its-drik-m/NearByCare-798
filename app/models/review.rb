fac54682eaab2fe251b3043ff1ddae98b250cccb
Cedric Marie <djced96@gmail.com>
2022-07-03 19:59:59 +0400

    Changed Review Model to assign reviews to one patient & one carer Only. Included possible rating in array

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,10 @@
 class Review < ApplicationRecord
+  RATING = [1, 2, 3, 4, 5]
+
   belongs_to :booking
+
+  has_one :carer, through: :booking
+  has_one :patient, through: :booking
 
   validates :comment, presence: true, length: { minimum: 5 }
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
