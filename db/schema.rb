555ebebb2f7ddb370aa0448d2a80742800213b06
ajagheh seeneevassen <agn_hs@yahoo.com>
2022-07-05 21:18:53 +0400

    finalising background color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_225105) do
+ActiveRecord::Schema.define(version: 2022_07_05_125830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,8 +63,6 @@ ActiveRecord::Schema.define(version: 2022_07_02_225105) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "region"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_carers_on_user_id"
   end
 
