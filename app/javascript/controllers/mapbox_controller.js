41bbaa69f038c1ced7995f61b88b2fece9c2a101
vanessa figaro <vfigaro02@gmail.com>
2022-07-06 10:05:28 +0400

    mapbox debugged

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
     apiKey: String,
     markers: Array
   }
+
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
     console.log("connected")
@@ -22,21 +23,22 @@ export default class extends Controller {
       zoom: 8
     })
     this.map.addControl(directions, "top-left");
-    this.#addMarkersToMap();
-    this.#fitMapToMarkers();
     navigator.geolocation.getCurrentPosition(position => {
       const { latitude, longitude } = position.coords;
       // window.location.reload()
       if (this.map.loaded()) {
         directions.setOrigin([longitude, latitude]);
         directions.setDestination([this.markersValue[0].lng, this.markersValue[0].lat]);
+        this.#addMarkersToMap();
       } else {
         this.map.on("load", () => {
           directions.setOrigin([longitude, latitude]);
           directions.setDestination([this.markersValue[0].lng, this.markersValue[0].lat]);
+          this.#addMarkersToMap();
         })
       }
-     })
+    })
+
     this.map.addControl(new mapboxgl.GeolocateControl({
       positionOptions: {
       enableHighAccuracy: true
@@ -46,6 +48,7 @@ export default class extends Controller {
       showUserLocation: true,
     }));
   }
+
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       console.log(marker)
@@ -53,10 +56,5 @@ export default class extends Controller {
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map)
     });
-  }
-  #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, duration: 200 })
   }
 }
