c8209b3a1fcf27101594672e864d70675254f3c9
vanessa figaro <vfigaro02@gmail.com>
2022-07-06 21:24:07 +0400

    debugging mapbox on heroku

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -17,10 +17,10 @@ export default class extends Controller {
     });
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/vanessaf23/cl59l8jpx001114o1jcmuwkrm",
+      style: "mapbox://styles/mapbox/streets-v10",
       center: [57.613891, -20.282669],
       zoom: 8
-    })
+    });
     this.map.addControl(directions, "top-left");
     navigator.geolocation.getCurrentPosition(position => {
       const { latitude, longitude } = position.coords;
@@ -29,14 +29,12 @@ export default class extends Controller {
         directions.setOrigin([longitude, latitude]);
         directions.setDestination([this.markersValue[0].lng, this.markersValue[0].lat]);
         this.#addMarkersToMap();
-        this.#fitMapToMarkers();
       } else {
         this.map.on("load", () => {
           directions.setOrigin([longitude, latitude]);
           directions.setDestination([this.markersValue[0].lng, this.markersValue[0].lat]);
           this.#addMarkersToMap();
-          this.#fitMapToMarkers();
-        })
+        });
       }
      })
     this.map.addControl(new mapboxgl.GeolocateControl({
@@ -51,20 +49,17 @@ export default class extends Controller {
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       console.log(marker)
+
       const customMarker = document.createElement("div")
       customMarker.className = "marker"
       customMarker.style.backgroundImage = `url('${marker.image_url}')`
       customMarker.style.backgroundSize = "contain"
-      customMarker.style.width = "25px"
-      customMarker.style.height = "25px"
+      customMarker.style.width = "50px"
+      customMarker.style.height = "50px"
+
       new mapboxgl.Marker(customMarker)
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map)
     });
-  }
-  #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, duration: 180 })
   }
 }
