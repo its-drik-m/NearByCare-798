ba9d7ff58962a41182fbab00814448c549e5a48b
vanessa figaro <vfigaro02@gmail.com>
2022-07-06 17:37:50 +0400

    readjusting tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_125830) do
+ActiveRecord::Schema.define(version: 2022_07_06_132933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,8 +72,8 @@ ActiveRecord::Schema.define(version: 2022_07_05_125830) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
-    t.float "latitude"
     t.float "longitude"
+    t.float "latitude"
     t.index ["user_id"], name: "index_patients_on_user_id"
   end
 
